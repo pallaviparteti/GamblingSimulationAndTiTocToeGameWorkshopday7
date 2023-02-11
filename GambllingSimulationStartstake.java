d9447a98d3de3caf77a5b197381bebcb6c26a795
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-02-11 17:30:05 +0530

    Added GambllingSimulationStartstake file

---
+++ GambllingSimulationStartstake.java
@@ -0,0 +1,21 @@
+package com.tictoctoeproblemandgambllingsimulation;
+import java.util.Scanner;
+
+public class GambllingSimulationStartstake {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("As a Gambler, would start with a stake of $100 every day and bet $1 every game. If Yes then type Yes else No type:");
+		String answer= sc.next();
+		if(answer=="Yes")
+		{
+			System.out.println("Welcome to Gambling World");
+		}
+		else
+		{
+			System.exit(0);
+		}
+	}
+}
+
+
